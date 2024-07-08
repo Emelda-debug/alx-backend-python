5468314856f294e350db34bb71b89558bb2532e9
Emelda-debug <teacayemmie@gmail.com>
2024-07-08 14:48:31 +0200

    more tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,6 +12,14 @@ wait_random = __import__(0 - basic_async_syntax.py).wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ async routine  that takes in 2 int arguments"""
-    task_list = [asyncio.create_task(wait_random(max_delay)) for x in range(n)]
-    completion = [await task for task in asyncio.as_completed(task_list)]
-    return completion
+    delay_time: List[float] = []
+    all_tasks: List = []
+
+    for _ in range(n):
+        all_tasks.append(wait_random(max_delay))
+
+    for task in asyncio.as_completed((all_tasks)):
+        delay_time = await task
+        delay_time.append(delay_time)
+
+    return delay_time
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,17 +6,18 @@ Returns total_time / n float
 """
 import time
 import asyncio
+from typing import List
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int = 0, max_delay: int= 10) -> float:
     """
     Function to measure_time and
     return float total_time / n
     """
     start_time = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    stop_time = time.perf_counter()
-    total_time = stop_time - start_time
-    return total_time / n
+    asyncio.run(wait_n(max_delay, n))
+    stop_time = time.perf_counter() - start_time
+    total_time = stop_time/n
+    return total_time
