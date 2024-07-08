92120d7c9c5b03b6011c861557d37ed13f5c216b
Emelda-debug <teacayemmie@gmail.com>
2024-07-08 16:58:36 +0200

    1 cor

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-""" async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay.
-and will spawn wait_random n times with the specified max_delay. """
+"""
+async routine called wait_n that takes in 2 int
+arguments (in this order): n and max_delay.
+and will spawn wait_random n times with the specified max_delay.
+"""
 import asyncio
 from typing import List
 
